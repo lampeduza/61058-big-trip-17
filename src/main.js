19eed590cf1aff41826b35361286293ea3432e91
meduza <lmdogg@mail.ru>
2022-04-30 22:39:12 +0700

    Презентер взялся за дело

--- src/main.js
+++ src/main.js
@@ -1,42 +1,14 @@
-// import LoadingView from './view/loading-view.js';
-import SortListView from './view/sort-view.js';
-import FilterView from './view/filter-view.js';
-import FilterMessageView from './view/filter-message-view.js';
-import TripListView from './view/trip-list-view.js';
-import TripItemView from './view/trip-item-view.js';
-import TripInfoView from './view/trip-info.js';
-import EditPointView from './view/edit-point-view.js';
-import NewPointWithoutOffersView from './view/new-point-without-offers-view.js';
-import NewPointWithoutDestinationView from './view/new-point-without-destination-view.js';
-import NewPointView from './view/new-point-view.js';
-
-import {createElement} from './render.js';
-
 import {render} from './render.js';
-// import Presenter from './presenter/presenter.js';
-// const presenter = new Presenter();
-// presenter.init(tripEvents);
+import {HeaderPresenter, BodyPresenter} from './presenter/presenter.js';
 
+const headerPresenter = new HeaderPresenter();
+const bodyPresenter = new BodyPresenter();
 
-// для header presenter
+// for header presenter
 const tripMain = document.querySelector('.trip-main');
 
-// Заголовок, дата, цена
-render(new TripInfoView(), tripMain, 'afterbegin');
-// Фильтры
-render(new FilterView(), tripMain.querySelector('.trip-controls__filters'));
-
-
-// для main presenter
+// for main presenter
 const tripEvents = document.querySelector('.trip-events');
 
-// Сортировка
-render(new SortListView(), tripEvents);
-render(new TripListView(), tripEvents);
-
-for (let i = 0; i < 3; i++) {
-	render(new TripItemView(), tripEvents.querySelector('.trip-events__list'));
-}
-
-// Новая точка
-render(new NewPointView(), tripEvents.querySelector('.trip-events__list'), 'afterbegin');
+headerPresenter.init(tripMain);
+bodyPresenter.init(tripEvents);
